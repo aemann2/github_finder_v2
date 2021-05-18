d4d69c528843a475d6bd313a3081efec39ed801c
Adam Mann <aemann2@gmail.com>
2021-05-17 20:40:22 -0700

    added bootstrap, navbar'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+import TopNav from './components/layout/Navbar';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <TopNav />
+    </div>
+  );
 }
 
 export default App;
