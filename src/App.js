4a03380c471c4173e33aaa8c008a12b4f80af470
Adam Mann <aemann2@gmail.com>
2021-05-18 10:28:19 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
-import TopNav from './components/layout/Navbar';
+import Navbar from './components/layout/Navbar/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <TopNav />
+      <Router>
+        <Navbar />
+      </Router>
     </div>
   );
 }
